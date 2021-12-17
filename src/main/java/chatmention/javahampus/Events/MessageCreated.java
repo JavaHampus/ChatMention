2adb450b51d940dd6a6ee515a02908fbbf3647e9
Hampus Olsson <HampusOlsson05@gmail.com>
2021-12-17 16:17:27 +0100

    LICENSE: Add MIT License

--- src/main/java/chatmention/javahampus/Events/MessageCreated.java
+++ src/main/java/chatmention/javahampus/Events/MessageCreated.java
@@ -1,7 +1,32 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2021-2020 JavaHampus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package chatmention.javahampus.Events;
 
 import chatmention.javahampus.ChatMention;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,8 +48,11 @@ public class MessageCreated implements Listener {
 
         for(UUID key : plugin.getDataStorage().getMentionStatus().keySet()) {
             if(Bukkit.getPlayer(key) != null) {
-                if(e.getMessage().contains(Bukkit.getPlayer(key).getName()) && plugin.getDataStorage().isPlayerMentionable(key)) {
-                    Bukkit.getPlayer(key).sendMessage("You have been mentioned in the chat.");
+                Player mentioned = Bukkit.getPlayer(key);
+
+                if(e.getMessage().contains("@" + mentioned.getName()) && plugin.getDataStorage().isPlayerMentionable(key)) {
+                    mentioned.sendMessage();
+                    mentioned.playSound(mentioned.getLocation(), Sound.BLOCK_GLASS_BREAK, 100, 100);
                 }
             }
         }
