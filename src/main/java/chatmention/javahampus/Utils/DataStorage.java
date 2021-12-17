b1a31c8aae80e168ddaa80fcc91ee7ca5db4f957
Hampus Olsson <HampusOlsson05@gmail.com>
2021-12-17 16:05:32 +0100

    JavaDoc: Documentation for DataStorage methods.

--- src/main/java/chatmention/javahampus/Utils/DataStorage.java
+++ src/main/java/chatmention/javahampus/Utils/DataStorage.java
@@ -11,18 +11,41 @@ public class DataStorage {
         return mentionStatus;
     }
 
+    /**
+     * Checks if player is mentionable in chat or not.
+     *
+     * @param uuid the player unique id.
+     * @return boolean from key value in HashMap.
+     */
     public boolean isPlayerMentionable(UUID uuid) {
         return mentionStatus.get(uuid);
     }
 
+    /**
+     * Checks if mentionsStatus HashMap contains key.
+     *
+     * @param uuid the player unique id.
+     * @return boolean if key exists in HashMap.
+     */
     public boolean containsPlayerKey(UUID uuid) {
         return mentionStatus.containsKey(uuid);
     }
 
+    /**
+     * Sets the value for a key in the mentionsStatus HashMap.
+     *
+     * @param uuid the player unique id.
+     * @param bool set player mentionable to a boolean.
+     */
     public void setPlayerMentionable(UUID uuid, Boolean bool) {
         mentionStatus.replace(uuid, bool);
     }
 
+    /**
+     * Creates key in mentionsStatus HashMap for a player.
+     *
+     * @param uuid the player unique id.
+     */
     public void createPlayerKey(UUID uuid) {
         mentionStatus.put(uuid, true);
     }
